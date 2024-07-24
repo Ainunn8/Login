fdb242f234a03dcddd65588c4b1be92a73981fc0
unknown <ainun9874@gmaiol.com>
2024-07-24 13:35:31 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    // Perform login logic here
+    console.log(Email: ${email}, Password: ${password});
+
+    alert('Login successful!');
+});
+
+// scripts.js
+
+document.getElementById('show-password-checkbox').addEventListener('change', function() {
+    var passwordInput = document.getElementById('password');
+    if (this.checked) {
+        passwordInput.type = 'text';
+    } else {
+        passwordInput.type = 'password';
+    }
+});
