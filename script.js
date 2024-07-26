2bdb5c37ddce99cf5285c2b62a2e9b4ec8d47d7a
unknown <ainun9874@gmaiol.com>
2024-07-26 08:23:30 +0700

    “update”

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
     
     // Perform login logic here
-    console.log(Email: ${email}, Password: ${password});
+    console.log(`Email: ${email}, Password: ${password}`);
 
     alert('Login successful!');
 });
 
-// scripts.js
+const passwordField = document.getElementById("password");
+const togglePassword = document.querySelector(".password-toggle-icon i");
 
-document.getElementById('show-password-checkbox').addEventListener('change', function() {
-    var passwordInput = document.getElementById('password');
-    if (this.checked) {
-        passwordInput.type = 'text';
-    } else {
-        passwordInput.type = 'password';
-    }
+togglePassword.addEventListener("click", function () {
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    togglePassword.classList.remove("fa-eye");
+    togglePassword.classList.add("fa-eye-slash");
+  } else {
+    passwordField.type = "password";
+    togglePassword.classList.remove("fa-eye-slash");
+    togglePassword.classList.add("fa-eye");
+  }
 });
